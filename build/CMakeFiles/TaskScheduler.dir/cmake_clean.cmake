675c0ad65b9b33aaa38f144c69331399f6dcbcfd
Andy <andres.meneguzzo@gmail.com>
2024-12-27 00:55:49 +0100

    built successfully with cmake

---
+++ build/CMakeFiles/TaskScheduler.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TaskScheduler.dir/main.cpp.o"
+  "CMakeFiles/TaskScheduler.dir/main.cpp.o.d"
+  "CMakeFiles/TaskScheduler.dir/src/PriorityQueue.cpp.o"
+  "CMakeFiles/TaskScheduler.dir/src/PriorityQueue.cpp.o.d"
+  "CMakeFiles/TaskScheduler.dir/src/Task.cpp.o"
+  "CMakeFiles/TaskScheduler.dir/src/Task.cpp.o.d"
+  "CMakeFiles/TaskScheduler.dir/src/TaskScheduler.cpp.o"
+  "CMakeFiles/TaskScheduler.dir/src/TaskScheduler.cpp.o.d"
+  "TaskScheduler"
+  "TaskScheduler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TaskScheduler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
