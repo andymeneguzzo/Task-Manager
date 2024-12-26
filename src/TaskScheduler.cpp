7217b5706102406d25f80f819382ec0658e892e4
Andy <andres.meneguzzo@gmail.com>
2024-12-26 13:05:13 +0100

    creating TaskScheduler.cpp

---
+++ src/TaskScheduler.cpp
@@ -0,0 +1,12 @@
+#include "TaskScheduler.h"
+#include <iostream>
+
+TaskScheduler::TaskScheduler(size_t numThreads) : running_(false) {
+    workers_.reserve(numThreads);
+}
+
+TaskScheduler::~TaskScheduler() {
+    stop();
+}
+
+void 
